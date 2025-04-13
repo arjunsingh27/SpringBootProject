0e58be12dd3b31d95987cde91cba111c050aae51
arjunsingh27 <arjunsingh15102003@gmail.com>
2025-04-14 04:18:07 +0530

    Update the My Sql Connection

---
+++ frontend/build/precache-manifest.2ec9d3499b4aa4f96c3c8eede00408b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61c41034c719b18e81d6086e38b8301a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53a35386b97d0218711f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "81ddcb598d33745c9fd6",
+    "url": "/static/js/2.324aded8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.324aded8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "53a35386b97d0218711f",
+    "url": "/static/js/main.6c604436.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  }
+]);
